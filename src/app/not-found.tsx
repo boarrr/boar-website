84ee6adfc780ee466ba01bc534fe7b1d532b7cd4
boarrr <inomoncatnip@hotmail.com>
2024-09-21 17:40:19 +0100

    react/no-unescaped-entities fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-4xl font-bold mb-4"> 404 | Page Not Found</h1>
-            <p className="text-lg mb-6">Sorry, we couldn't find the page you're looking for.</p>
+            <p className="text-lg mb-6">Sorry, we couldn&#39;t find the page you&#39;re looking for.</p>
             <a href="/" className="text-red-400 font-bold underline">
                 Go back to Home
             </a>
